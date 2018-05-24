9b1ea37bf33fec32d1e4b80accc18fd5283ccd8c
Francis Menkes <francis.menkes@gmail.com>
2018-05-24 20:06:27 +0200

    Use .env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
 import * as firebase from 'firebase';
   // Initialize Firebase
   const prodConfig = {
-    apiKey: "AIzaSyAa_o6H3NFHsEQXDyRlkrpHMkSJX2M4fuk",
+    apiKey: process.env.REACT_APP_FIREBASE_PROD_API_KEY,
     authDomain: "fotbolls-vm-2018.firebaseapp.com",
     databaseURL: "https://fotbolls-vm-2018.firebaseio.com",
     projectId: "fotbolls-vm-2018",
@@ -10,7 +10,7 @@ import * as firebase from 'firebase';
   };
   
   const devConfig = {
-      apiKey: "AIzaSyAZjMb7Sc9LNrlByKVNGAvoxg5Xj0_Z4qo",
+      apiKey: process.env.REACT_APP_FIREBASE_DEV_API_KEY,
       authDomain: "fotbolls-vm-2018-dev.firebaseapp.com",
       databaseURL: "https://fotbolls-vm-2018-dev.firebaseio.com",
       projectId: "fotbolls-vm-2018-dev",
