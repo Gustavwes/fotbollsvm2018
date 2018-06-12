16554d068389741cb855cdbbae0813e1d43aff13
Gustav Westberg <31844570+Gustavwes@users.noreply.github.com>
2018-06-12 21:25:43 +0200

    sort of functioning forms

--- fotbollsvm2018/src/state/reducers/index.js
+++ fotbollsvm2018/src/state/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form'; 
 
 import games from './games';
 
 export default combineReducers({
     games,
     routing: routerReducer,
+    form: formReducer
 });
