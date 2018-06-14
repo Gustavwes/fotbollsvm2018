3e8362f6684fb004d1d05bc22214e040c8a7d440
Gustav Westberg <31844570+Gustavwes@users.noreply.github.com>
2018-06-14 15:35:39 +0200

    stats=>new table, unique colors foreach user

--- fotbollsvm2018/src/components/Stats.js
+++ fotbollsvm2018/src/components/Stats.js
@@ -3,18 +3,27 @@ import { connect } from 'react-redux';
 import { db, firebase } from '../firebase'
 import '../../node_modules/react-vis/dist/style.css';
 import { DiscreteColorLegend, XYPlot, LineMarkSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis } from 'react-vis';
-
 import { getGames } from '../state/actions/games';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const matchDays  = ['1','2','3','Round of 16','Quarter Finals', 'Semi Finals', 'Bronze Match', 'Finals'];
-const examplePoints = [0,2,3,4,5,6,4,5];
-const fakeMatches =  [
-    {id: '_165069', matchday:  1, teamAResult: 1, teamBResult: 0},
-    {id: '_165072', matchday: 1, teamAResult: 2, teamBResult: 2},
-    {id: '_165073', matchday: 1, teamAResult: 2, teamBResult: 4},
-    {id: '_165076', matchday: 1, teamAResult: 3, teamBResult: 2},
-    {id: '_165092', matchday: 2, teamAResult: 4, teamBResult: 2}    
-]
+const colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
 class Stats extends React.Component {
     constructor(props) {
         super(props);
@@ -26,6 +35,7 @@ class Stats extends React.Component {
             currentUser: {},
             users: {},
             bets: {},
+            classes: props,
         }
         this.createDataSet = this.createDataSet.bind(this);
         this.calculatePoints = this.calculatePoints.bind(this);
@@ -33,8 +43,7 @@ class Stats extends React.Component {
     };
     
     createDataSet(user){
-        var test = this.calculatePoints(user.id);  
-        console.log(test);      
+        var test = this.calculatePoints(user.id);      
         let userDataSet = {};
         let dataPoints = [
             { x: matchDays[0], y: test[0].points },
@@ -93,7 +102,6 @@ class Stats extends React.Component {
                 if(matchDayTotalPoints!==0){
                     matchdayObj.points = totalPoints + matchDayTotalPoints;
                     totalPoints = totalPoints + matchDayTotalPoints;
-                    console.log(totalPoints);
                 }else{
                     matchdayObj.points = totalPoints;
                 }                   
@@ -115,7 +123,7 @@ class Stats extends React.Component {
         this.state.userArray.forEach((user)=>{
             this.createDataSet(user);
         });
-        this.setState({lineMarkers: this.state.userDataSetArray.map(user => <LineMarkSeries key={user.username} data={user.points} />)}); 
+        this.setState({lineMarkers: this.state.userDataSetArray.map((user, index) => <LineMarkSeries key={user.username} data={user.points} color={colorArray[index]}/>)}); 
         this.setState({usersNames: this.state.userDataSetArray.map(user => user.username)});         
         
         await firebase.auth.onAuthStateChanged(authUser => {
@@ -133,19 +141,56 @@ class Stats extends React.Component {
         return (
             <div className="container">
                 <div className="marginDiv" />
-                <XYPlot height={600} width={800} xType={'ordinal'}>
-                    
+                <XYPlot height={600} width={800} xType={'ordinal'} >                    
                     <VerticalGridLines />
                     <HorizontalGridLines />
                     <XAxis title="Matchday" />  
                     <YAxis title="Points" />
                     { this.state.lineMarkers }
                 </XYPlot>
-                <DiscreteColorLegend width={180} items={this.state.usersNames} /> 
+                <DiscreteColorLegend orientation="horizontal" width={1200} items={this.state.usersNames}  colors={colorArray}/>                 
+                    
+                <Paper className={this.state.classes.root}>
+                <Table className={this.state.classes.table}>
+                    <TableHead>
+                    <TableRow>
+                        <CustomTableCell>Current Points</CustomTableCell>
+                        <CustomTableCell numeric>Matchday 1</CustomTableCell>
+                        <CustomTableCell numeric>Matchday 2</CustomTableCell>
+                        <CustomTableCell numeric>Matchday 3</CustomTableCell>
+                        <CustomTableCell numeric>Round of 16</CustomTableCell>
+                        <CustomTableCell numeric>Quarter-Finals</CustomTableCell>
+                        <CustomTableCell numeric>Semi-Finals</CustomTableCell>
+                        <CustomTableCell numeric>Bronze Match</CustomTableCell>
+                        <CustomTableCell numeric>Final</CustomTableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {this.state.userDataSetArray.map(n => {
+                        return (
+                        <TableRow className={this.state.classes.row} key={n.username}>
+                            <CustomTableCell component="th" scope="row">
+                            {n.username}
+                            </CustomTableCell>
+                            <CustomTableCell numeric>{n.points[0].y}</CustomTableCell>
+                            <CustomTableCell numeric>{n.points[1].y}</CustomTableCell>
+                            <CustomTableCell numeric>{n.points[2].y}</CustomTableCell>
+                            <CustomTableCell numeric>{n.points[3].y}</CustomTableCell>
+                            <CustomTableCell numeric>{n.points[4].y}</CustomTableCell>
+                            <CustomTableCell numeric>{n.points[5].y}</CustomTableCell>
+                            <CustomTableCell numeric>{n.points[6].y}</CustomTableCell>
+                            <CustomTableCell numeric>{n.points[7].y}</CustomTableCell>
+                        </TableRow>
+                        );
+                    })}
+                    </TableBody>
+                </Table>
+                </Paper>
             </div>
         );
     }
 }
+
 
 const mapStateToProps = state => ({
     games: state.games.games,
@@ -154,6 +199,31 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     getGames: () => dispatch(getGames()),
 });
+const CustomTableCell = withStyles(theme => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
 
+  
+  const styles = theme => ({
+    root: {
+      width: '100%',
+      marginTop: theme.spacing.unit * 3,
+      overflowX: 'auto',
+    },
+    table: {
+      minWidth: 700,
+    },
+    row: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.background.default,
+      },
+    },
+  });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Stats);
